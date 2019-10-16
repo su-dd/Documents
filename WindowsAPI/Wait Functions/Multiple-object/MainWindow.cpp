6560fdc776a952837e3429a227b91c4b275e1151
sudaidai <987380454@qq.com>
2019-10-16 17:08:20 +0800

    提交 Multiple-object 实例

---
+++ WindowsAPI/Wait Functions/Multiple-object/MainWindow.cpp
@@ -0,0 +1,68 @@
+#include "MainWindow.h"
+#include <QApplication>
+#include <QPushButton>
+#include <QMessageBox>
+#include "WinUser.h"
+
+TestThread::TestThread(HANDLE event):
+    m_event(event)
+{
+
+}
+
+void TestThread::run()
+{
+    Sleep(10000);
+    SetEvent(m_event);
+}
+
+MainForm::MainForm(QWidget *parent)
+    : QMainWindow(parent),
+      m_event(nullptr)
+{
+    resize(400, 300);
+
+    auto btn1 = new QPushButton("test1", this);
+    connect(btn1, &QPushButton::clicked, this, &MainForm::onTest1);
+
+    auto btn2 = new QPushButton("test2", this);
+    connect(btn2, &QPushButton::clicked, this, &MainForm::onTest2);
+    btn2->move(btn1->width() + 10, btn2->y());
+
+    m_event = CreateEvent(nullptr, TRUE, FALSE, nullptr);
+}
+
+MainForm::~MainForm()
+{
+    CloseHandle(m_event);
+}
+
+void MainForm::onTest1()
+{
+    HANDLE aEvents[1];
+    aEvents[0] = m_event;
+
+    TestThread testThread(m_event);
+    testThread.start();
+
+    while (true)
+    {
+        DWORD waitResult = MsgWaitForMultipleObjects(1, aEvents, FALSE, INFINITE, QS_ALLEVENTS);
+        if (waitResult == WAIT_OBJECT_0)
+        {
+            QMessageBox::about(this, "MainForm::onTest1", "TestThread set Event.");
+            break;
+        }
+        else
+        {
+            qApp->processEvents();
+        }
+    }
+
+    QMessageBox::about(this, "MainForm::onTest1", "onTest1 OK!");
+}
+
+void MainForm::onTest2()
+{
+    QMessageBox::about(this, "MainForm::onTest2", "onTest2 OK!");
+}
---
+++ WindowsAPI/Wait Functions/Multiple-object/MainWindow.h
@@ -0,0 +1,31 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QThread>
+#include "Windows.h"
+
+class TestThread : public QThread
+{
+public:
+    TestThread(HANDLE event);
+protected:
+    void run() override;
+private:
+    HANDLE m_event;
+};
+
+class MainForm : public QMainWindow
+{
+    Q_OBJECT
+public:
+    MainForm(QWidget *parent = 0);
+    ~MainForm();
+private:
+    void onTest1();
+    void onTest2();
+
+    HANDLE m_event;
+};
+
+#endif // MAINWINDOW_H
