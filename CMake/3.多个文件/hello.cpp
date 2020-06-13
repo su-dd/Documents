9a0b7b1b0f119c036cf2ca703a541ae32c301539
daidai <987380454@qq.com>
2020-06-13 20:32:14 +0800

    添加cmake多个文件夹的情况

---
+++ CMake/3.多个文件/hello.cpp
@@ -0,0 +1,9 @@
+#include "hello.h"
+#include <iostream>
+
+using namespace std;
+
+void hello::say()
+{
+    cout << "say hello !" << endl;
+}
---
+++ CMake/4.多个文件夹/main.cpp
@@ -0,0 +1,9 @@
+#include "hello/hello.h"
+#include "stdio.h"
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    hello().say();
+    return 0;
+}
