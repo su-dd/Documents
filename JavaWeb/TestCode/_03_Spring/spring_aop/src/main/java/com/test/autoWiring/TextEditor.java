c209427dfe6d06458389223eb2bee99750c0a7b3
su-dd <987380454@qq.com>
2020-10-09 18:09:31 +0800

    demo学习

---
+++ JavaWeb/TestCode/_03_Spring/spring_aop/src/main/java/com/test/autoWiring/TextEditor.java
@@ -0,0 +1,31 @@
+package com.test.autoWiring;
+
+public class TextEditor {
+    private SpellChecker spellChecker;
+    private String name;
+
+    public TextEditor(SpellChecker spellChecker, String name) {
+        this.spellChecker = spellChecker;
+        this.name = name;
+    }
+
+    public SpellChecker getSpellChecker() {
+        return spellChecker;
+    }
+
+    public void setSpellChecker(SpellChecker spellChecker) {
+        this.spellChecker = spellChecker;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void spellCheck() {
+        spellChecker.checkSpelling();
+    }
+}
